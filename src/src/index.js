0983cbdf883dfcf5a1a47fb5725d982c86c15893
guillaumeduhan <guillaume@airdeguillaume.home>
2024-02-15 19:13:40 +0100

    commit

--- src/src/index.js
+++ src/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import UI from 'useritem';
 import UserItem from './components/UserItem';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode>
-  {/* <UI infos={true} title="Guillaume Duhan" description="Chief Technology Officer" backgroundColor="#323444" /> */}
   <UserItem title="Guillaume Duhan" backgroundColor="#323444" noPadding />
 </React.StrictMode>);
