c2ef1352ef1d9856aaecc31b1b12943d149b045b
guillaumeduhan <guillaume@MacBook-Air-de-Guillaume.local>
2024-03-03 16:00:00 +0400

    commit

--- React/src/index.js
+++ React/src/index.js
@@ -4,5 +4,5 @@ import UserItem from './components/UserItem';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode>
-  <UserItem title="Guillaume Duhan" description="Youtuber @codewithguillaume" status online maxWidth={400} verified />
+  <UserItem title="Guillaume Duhan" description="Youtuber @codewithguillaume" status online maxWidth={400} />
 </React.StrictMode>);
