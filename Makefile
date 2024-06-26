e2cd9992958cb3da25e8ac70ae10b05cf752b2d3
guillaumeduhan <guillaume@MacBook-Air-de-Guillaume.local>
2024-06-27 00:10:21 +0200

    commit

--- Makefile
+++ Makefile
@@ -2,3 +2,10 @@ gt:
 	git add .
 	git commit -m "commit"
 	git push
+
+gm:
+	git checkout main
+	git pull
+
+publish:
+	yarn version --patch && npm i && npm run build && make gt && npm publish
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import UserItem from './UserItem.jsx'
+// import UserItem from './UserItem.jsx'
+import UserItem from "useritem"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
