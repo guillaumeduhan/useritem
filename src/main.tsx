42f92be6a0baf9cc0595eea100781367af12c0f9
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2024-07-17 19:04:00 +0200

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import UserItem from './UserItem'
-// import UserItem from "useritem"
+// import UserItem from './UserItem'
+import UserItem from "useritem"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
